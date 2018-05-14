1ad644f1a106ff27b3879ea850c079129f04c39c
Omar Abdelkader <omikader@gmail.com>
2018-05-14 14:17:44 -0400

    Update documentation for deep learning script

--- src/deep_learning.py
+++ src/deep_learning.py
@@ -44,21 +44,20 @@ class LeNet5(nn.Module):
         x = F.max_pool2d(F.relu(self.conv1(x)), kernel_size=(2,2))
         x = F.max_pool2d(F.relu(self.conv2(x)), kernel_size=(2,2))
 
-        # Use softmax over MSE for classification loss
+        # Use cross-entropy instead of MSE for classification loss
         x = x.view(-1, 400)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
+# Function for splitting training data into training and validation sets
 def get_train_valid_loader(random_seed, valid_size=0.1, shuffle=True):
     error_msg = '[!] valid_size should be in the range [0, 1].'
     assert ((valid_size >= 0) and (valid_size <= 1)), error_msg
 
-    # define transforms
     transform = transforms.Compose([transforms.ToTensor()])
 
-    # load the dataset
     train_dataset = datasets.MNIST(
         root='../data', train=True, download=True, transform=transform)
     valid_dataset = datasets.MNIST(
@@ -83,8 +82,8 @@ def get_train_valid_loader(random_seed, valid_size=0.1, shuffle=True):
 
     return train_loader, valid_loader
 
+# Function to loading the testing dataset
 def get_test_loader(shuffle=True):
-    # define transform
     transform = transforms.Compose([transforms.ToTensor()])
 
     test_dataset = datasets.MNIST(
@@ -100,8 +99,8 @@ def train(model, optimizer, epoch, loader):
         data, target = Variable(data), Variable(target)
         optimizer.zero_grad() # Removes all past gradient run info (clean up)
         output = model(data) # Runs the model
-        loss = F.nll_loss(output, target) # Computes the loss function (negative loss of the softmax)
-        loss.backward() # Backprop
+        loss = F.nll_loss(output, target) # Computes the loss function (negative log-loss of the softmax output)
+        loss.backward() # Run backprop to update weights
         optimizer.step() # Take a step in the right direction of the SGD
 
         # Logging
@@ -118,7 +117,7 @@ def test(model, loader, name):
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data) # Runs the model
         test_loss += F.nll_loss(output, target, size_average=False).data[0] # Sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1] # Get the index of the max log-probability
+        pred = output.data.max(1, keepdim=True)[1] # Get the index (label) of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).long().cpu().sum()
 
     # Logging
@@ -133,7 +132,7 @@ def test(model, loader, name):
 # Set manual seed to create reproduceable results
 torch.manual_seed(args.seed)
 
-# Create new LeNet model and stochastic gradient descent optimization algo
+# Create new LeNet model and stochastic gradient descent optimization object
 model = LeNet5()
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
