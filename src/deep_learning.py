010f69ae884d94f9f399ad3eb55d450ba9eaf187
Omar Abdelkader <omikader@gmail.com>
2018-05-14 14:28:16 -0400

    Remove unused import

--- src/deep_learning.py
+++ src/deep_learning.py
@@ -6,7 +6,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from copy import deepcopy
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 from torch.utils.data.sampler import SubsetRandomSampler
